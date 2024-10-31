b809993f97ad1273a544dbb557a32e7ea7595744
Your Name <azmi.bengkalis@gmail.com>
2024-11-01 05:58:43 +0700

    blade init

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::get('reports');
+Route::get('reports/create');
+Route::post('reports');
+Route::get('reports/show/{id}');
+Route::get('reports/edit/{id}');
+Route::put('reports/show/{id}');
+Route::delete('reports/{id}');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+// Route::view('/','home');
+// Route::get('/', fn() => view('home'));
+Route::get('/', [Controllers\ApiController::class,'index']);
+Route::get('/about', [Controllers\AboutController::class,'about']);
+Route::get('/contact', fn() => view('pages.contact'));
+Route::get('/gallery', fn() => view('pages.gallery'));
+// Route::get('/', fn() => view('home'))->name('home');
+
+
+
